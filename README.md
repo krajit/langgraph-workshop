7bdbe7f925ec20f16696e5dc354cb4f6e5d8143b
Ajit Kumar <ajitkumar22@gmail.com>
2025-10-19 16:25:48 +0530

    simple graph created

---
+++ README.md
@@ -0,0 +1,18 @@
+# A simple graph
+- one node
+- simple conversation
+- persistent memory
+- `%%writefile` magic used to avoid separate code typing in notebook and for studio
+
+[notebooks\nb01.ipynb]
+
+
+# Exercise: Add a summarizer node
+
+Graphs requirements:
+- simple chatbot
+- create a summarizer node
+- After each turn of conversation, the summarizer node should summarize the converstation.
+
+
+# Exercise: Create an agent with a web search tool
--- notebooks/studio/simpleGraph.py
+++ notebooks/studio/simpleGraph.py
@@ -9,14 +9,13 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-
+from langgraph.checkpoint.memory import MemorySaver  # Add this import
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
 graph_builder = StateGraph(State)
-
 
 llm = init_chat_model("anthropic:claude-3-5-sonnet-latest")
 
@@ -31,4 +30,8 @@ def chatbot(state: State):
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
-graph = graph_builder.compile()
+
+
+# Add checkpointer for persistence
+checkpointer = MemorySaver()
+graph = graph_builder.compile(checkpointer=checkpointer)  # Add checkpointer her
